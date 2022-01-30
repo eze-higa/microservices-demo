843ebf1732bda8aa426c867055610b172fed8823
eze-higa <53649465+eze-higa@users.noreply.github.com>
2022-01-30 11:16:52 -0300

    Update Program.cs
    add injection of httpClient and commandClient

--- src/PlatformService/Program.cs
+++ src/PlatformService/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
 using PlatformService.Data.Repositories;
 using PlatformService.Models;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,9 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 
 builder.Services.AddScoped<IRepository<Platform>, Repository<Platform>>();
+
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+builder.Services.AddScoped<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
